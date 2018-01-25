64196d17ca10eb77592c81ffebf9f320a8ddd1d8
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 22:00:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function getCommits(el) {
   req.send()
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories); //when the event fires showRepositories() gets called.
