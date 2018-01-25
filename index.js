ae322933def2166561b378150f22e0367335c23b
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 22:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,3 @@ function showCommits() {
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
-
-
