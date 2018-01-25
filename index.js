abde82685b93b2d4b66af47bd0b321fae618a4b8
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 21:56:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function showRepositories(event, data) {
   //this is set to the XMLHttpRequest object that fired the event
   var repos = JSON.parse(this.responseText) //this tells the program that it is working with a JSON object
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
