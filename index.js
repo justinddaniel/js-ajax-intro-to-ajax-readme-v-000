b87dccfca92a0a531bb9228bccafd1a15f1d0109
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 21:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ function showRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList
 }
 
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories); //when the event fires showRepositories() gets called.
   req.open("GET", 'https://api.github.com/users/justinddaniel/repos')
   req.send()
 }
+
